a12ace1aa2cc072216cafd19401e3ea7f5acf80e
Matthew Llerena <140498422+Cromo0o@users.noreply.github.com>
2025-09-29 20:53:11 -0500

    Add files via upload

---
+++ Suma de dos numeros.cpp
@@ -0,0 +1,14 @@
+//suma de dos numeros entero
+
+#include <iostream>
+
+int main(){
+
+    int num1 = 2;
+    int num2 = 3;
+    int resultado = num1 + num2;
+
+    std::cout << num1 << " + " << num2 << " = " << resultado;
+
+    return 0;
+}
